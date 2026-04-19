70f8244dcc3e75c048d7008f722389173a609e26
Hashim Hassan <hashimhassan720@gmail.com>
2026-04-19 16:44:48 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
 if "shopping_list" not in st.session_state:
     st.session_state.shopping_list = []
 
+if "checked_items" not in st.session_state:
+    st.session_state.checked_items = set()
+
 st.title("🛒 Shopping List Manager")
 
 menu = st.radio("Choose an option:", ["Add item", "Delete item", "Show list"])
@@ -21,7 +24,7 @@ if menu == "Add item":
                 st.warning("Item already in list")
             else:
                 st.session_state.shopping_list.append(user_item)
-                st.success(f"{user_item} added!")
+                st.success("item added")  # green message
 
 # DELETE ITEM
 elif menu == "Delete item":
@@ -32,19 +35,24 @@ elif menu == "Delete item":
         if submitted:
             if user_item in st.session_state.shopping_list:
                 st.session_state.shopping_list.remove(user_item)
-                st.success(f"{user_item} removed!")
+                st.session_state.checked_items.discard(user_item)
+                st.error("item removed")  # red message
             else:
                 st.warning("Item not found")
 
 # SHOW LIST
 elif menu == "Show list":
-    with st.form("show_form"):
-        submitted = st.form_submit_button("Show list")
+    if len(st.session_state.shopping_list) == 0:
+        st.info("Your list is empty")
+    else:
+        st.subheader("Your Shopping List:")
 
-        if submitted:
-            if len(st.session_state.shopping_list) == 0:
-                st.info("Your list is empty")
+        for item in st.session_state.shopping_list:
+            checked = item in st.session_state.checked_items
+
+            new_value = st.checkbox(item, value=checked, key=item)
+
+            if new_value:
+                st.session_state.checked_items.add(item)
             else:
-                st.subheader("Your Shopping List:")
-                for i, item in enumerate(st.session_state.shopping_list, start=1):
-                    st.write(f"{i}. {item}")
+                st.session_state.checked_items.discard(item)
